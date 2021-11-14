cf0976a37a32b29fe380748484a0c0430888d0b4
me <tismee@gmail.com>
2021-11-14 07:03:52 +0300

    JUnit5 test class added
    'CashbackHackServiceTest' class had to be renamed to: 'JUnit4_CashbackHackServiceTest'

--- src/test/java/ru/netology/unit/JUnit5_CashbackHackServiceTest.java
+++ src/test/java/ru/netology/unit/JUnit5_CashbackHackServiceTest.java
@@ -1,25 +1,30 @@
 package ru.netology.unit;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static ru.netology.unit.CashbackHackService.BOUNDARY;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 /**
- * @version JUnit4-Jupiter
+ * @version JUnit Jupiter
  */
-public class CashbackHackServiceTest {
+public class JUnit5_CashbackHackServiceTest {
+
     private final CashbackHackService service = new CashbackHackService();
 
-    @Test public void shouldReturn100() {
+    @Test
+    public void shouldReturn100() {
         assertEquals(100, service.calculateRemainder(BOUNDARY - 100));
     }
 
-    @Test public void shouldReturnZero() {
+    @Test
+    public void shouldReturnZero() {
         assertEquals(0, service.calculateRemainder(BOUNDARY));
     }
 
-    @Test public void shouldReturn900() {
+    @Test
+    public void shouldReturn900() {
         assertEquals(900, service.calculateRemainder(BOUNDARY + 100));
     }
 }
+
