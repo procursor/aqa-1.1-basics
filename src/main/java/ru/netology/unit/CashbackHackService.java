b6708104cece9fff88bb9ac88050c7d64085e5b4
me <tismee@gmail.com>
2021-11-14 18:03:00 +0300

    refactoring: JUnit4 & JUnit5 test classes moved into separate packages
    * Renamed test classes back to 'CashbackHackServiceTest'. Now each of them belongs to its own package
    
    * Provided some basic logging facility `LOGGER`
    
    * Made `BOUNDARY` constant public because it is rather class var than instance var. Also, for some reasons JUnit4 of the class couldn't import it without making it private in the service class.

--- src/main/java/ru/netology/unit/CashbackHackService.java
+++ src/main/java/ru/netology/unit/CashbackHackService.java
@@ -1,10 +1,10 @@
 package ru.netology.unit;
 
 /**
- * @version 0.1
+ * @version 0.1.1
  */
 public class CashbackHackService {
-    final static int BOUNDARY = 1000;   // defines cashback _threshold_
+    public final static int BOUNDARY = 1000;   // defines cashback _threshold_
 
     public int calculateRemainder(int amount) {
         return BOUNDARY - amount % BOUNDARY;
---
+++ src/test/java/ru/netology/unit/JUnit5/CashbackHackServiceTest.java
@@ -0,0 +1,47 @@
+package ru.netology.unit.JUnit5;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.logging.Logger;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
+import ru.netology.unit.CashbackHackService;
+
+/**
+ * @version JUnit5
+ */
+public class CashbackHackServiceTest {
+
+    private static final String engine = "JUnit5";
+    final CashbackHackService service = new CashbackHackService();
+
+    private static final Logger LOGGER = Logger.getLogger(CashbackHackService.class.getName());
+
+    @BeforeAll
+    public static void init() {
+        LOGGER.info(engine + " platform tests are running:");
+    }
+
+    @BeforeEach
+    void setup(TestInfo testInfo) {
+        LOGGER.info("About to execute " + engine + ": " + testInfo.getDisplayName());
+    }
+
+    @Test
+    public void shouldReturn100() {
+        assertEquals(100, service.calculateRemainder(CashbackHackService.BOUNDARY - 100));
+    }
+
+    @Test
+    public void shouldReturnZero() {
+        assertEquals(0, service.calculateRemainder(CashbackHackService.BOUNDARY));
+    }
+
+    @Test
+    public void shouldReturn900() {
+        assertEquals(900, service.calculateRemainder(CashbackHackService.BOUNDARY + 100));
+    }
+}
+
